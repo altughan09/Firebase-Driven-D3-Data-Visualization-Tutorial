8c69323a5508e7fd2ad7342784c4b89664647a91
Altughan Ozengi <31768522+altugozengi@users.noreply.github.com>
2019-08-21 12:01:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", 600)
+  .attr("height", 600);
+
+const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg
+  .append("g")
+  .attr("width", graphWidth)
+  .attr("height", graphHeight)
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+const xAxisGroup = graph
+  .append("g")
+  .attr("transform", `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append("g");
+
+db.collection("dishes")
+  .get()
+  .then(res => {
+    let data = [];
+    res.docs.forEach(doc => {
+      data.push(doc.data());
+    });
+
+    const rects = graph.selectAll("rect").data(data);
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.orders)])
+      .range([graphHeight, 0]);
+
+    const min = d3.min(data, d => d.orders);
+    const max = d3.max(data, d => d.orders);
+    const extent = d3.extent(data, d => d.orders);
+
+    const x = d3
+      .scaleBand()
+      .domain(data.map(item => item.name))
+      .range([0, 500])
+      .paddingInner(0.2)
+      .paddingOuter(0.2);
+
+    rects
+      .attr("width", x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr("fill", "green")
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.orders));
+
+    rects
+      .enter()
+      .append("rect")
+      .attr("width", x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr("fill", "green")
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.orders));
+
+    //create and call the axes
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3
+      .axisLeft(y)
+      .ticks(3)
+      .tickFormat(d => d + " orders");
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+    xAxisGroup
+      .selectAll("text")
+      .attr("transform", "rotate(-20)")
+      .attr("text-anchor", "end")
+      .attr("fill", "crimson");
+  });
